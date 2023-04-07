e7502fcd7d0fa74fd8b23736bbae890e7806324f
veekeetee <gavriviktoriia@gmail.com>
2023-04-07 23:15:26 +0300

    add_test_group добавка парам в Логин и Креэйт групп

--- AddTestGroupFX.py
+++ AddTestGroupFX.py
@@ -4,50 +4,44 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
+
 class AddTestGroupFX(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-
     def test_add_test_group_f_x(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd)
-        self.return_to_groups_page(wd)
+        self.open_groups_name(wd)
+        self.create_group(wd, name="12345", header="54321", footer="12345")
+        self.return_to_group_page(wd)
         self.logout(wd)
-
-    def create_group(self, wd):
-        self.init_group_creation(wd)
-        self.fill_group_form(wd)
-        self.submit_group_creation(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def return_to_groups_page(self, wd):
+    def return_to_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def submit_group_creation(self, wd):
-        wd.find_element_by_name("submit").click()
-
-    def fill_group_form(self, wd):
+    def create_group(self, wd, name, header, footer):
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group firm
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("12345")
+        wd.find_element_by_name("group_name").send_keys("%s" % name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("54321")
+        wd.find_element_by_name("group_header").send_keys("%s" % header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("12345")
+        wd.find_element_by_name("group_footer").send_keys("%s" % footer)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
 
-    def init_group_creation(self, wd):
-        wd.find_element_by_name("new").click()
-
-    def open_groups_page(self, wd):
+    def open_groups_name(self, wd):
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
@@ -60,17 +54,6 @@ class AddTestGroupFX(unittest.TestCase):
 
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
 
     def tearDown(self):
         self.wd.quit()
