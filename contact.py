7c89783fe931839d276d210a29ea41adc7128efa
veekeetee <gavriviktoriia@gmail.com>
2023-04-10 15:39:55 +0300

    add_test_contact Рабочий тест 1.4 с учетом правок от тренера.

--- contact.py
+++ contact.py
@@ -1,6 +1,6 @@
 class Contact:
-    def __init__(self, firstname, middlename, lastname, nickname, tittle, company, address, home, mobile,
-                       work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2,
+    def __init__(self, firstname, middlename, lastname, nickname, tittle, company, address, homephone, mobile,
+                       workphone, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2,
                        phone2, notes):
         self.firstname = firstname
         self.middlename = middlename
@@ -9,9 +9,9 @@ class Contact:
         self.tittle = tittle
         self.company = company
         self.address = address
-        self.home = home
+        self.homephone = homephone
         self.mobile = mobile
-        self.work = work
+        self.workphone = workphone
         self.fax = fax
         self.email = email
         self.email2 = email2
