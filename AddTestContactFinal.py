d32e994ed4e0a0154247e782075e4f65663dde26
veekeetee <gavriviktoriia@gmail.com>
2023-04-11 23:56:48 +0300

    add_test_contact Рабочий тест 3.4 этап2- переход на pytest

--- AddTestContactFinal.py
+++ AddTestContactFinal.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+
+import pytest
 from contact import Contact
 from applying import Applying
 
-class AddTestContact(unittest.TestCase):
-    def setUp(self):
-        self.app = Applying()
+@pytest.fixture
+def app(request):
+    fixture = Applying()
+    request.addifinalizer(fixture.destroy)
+    return fixture
 
 
-    def test_add_test_contact(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_contact(Contact(firstname="Volodya", middlename="Petrovich", lastname="Volkov", nickname="Volk", tittle="Pik", company="Puki", address="gorod", homephone="9-999-99",
+def test_add_test_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="Volodya", middlename="Petrovich", lastname="Volkov", nickname="Volk", tittle="Pik", company="Puki", address="gorod", homephone="9-999-99",
                             mobile="9-999-999-99-99", workphone="9-99", fax="9", email="volkov@volkov", email2="volkov2@volkov", email3="volkov3@volkov",
                             homepage="volkov0volkov", bday="21", bmonth="December", byear="2000", aday="31", amonth="July", ayear="2010", address2="gorod2", phone2="8-888-88",
                             notes="pipipuk"))
-        self.app.logout()
+    app.logout()
 
 
-    def tearDown(self):
-        self.app.destroy()
 
-
-if __name__ == "__main__":
-    unittest.main()
