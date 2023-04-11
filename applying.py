ab8ddbba48352c9f81fd2bea5dfe824f2fb7bd03
veekeetee <gavriviktoriia@gmail.com>
2023-04-12 00:30:49 +0300

    add_test_contact Рабочий тест 3.4 фикстура в отдельном классе

--- applying.py
+++ applying.py
@@ -5,6 +5,7 @@ class Applying:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
