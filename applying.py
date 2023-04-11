31491f54c6ab5760149d9ef475afc1eb553e8ede
veekeetee <gavriviktoriia@gmail.com>
2023-04-12 00:34:24 +0300

    add_test_contact Рабочий тест 3.4 фикстура в отдельном классе

--- applying.py
+++ applying.py
@@ -4,8 +4,8 @@ from selenium.webdriver.support.ui import Select
 class Applying:
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-        
+        self.wd.implicitly_wait(40)
+
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
