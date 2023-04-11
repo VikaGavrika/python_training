3b315c2e27c4cca477ff13d7a0287fceeb9cecc9
veekeetee <gavriviktoriia@gmail.com>
2023-04-11 23:14:03 +0300

    add_test_contact Рабочий тест 3.1 методы переходов включила во вспомогательные методы (убрала с тестовых методов)

--- AddTestContactFinal.py
+++ AddTestContactFinal.py
@@ -11,20 +11,18 @@ class AddTestContact(unittest.TestCase):
 
     def test_add_test_contact(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_add_new_contact_page(wd)
         self.create_contact(wd, Contact(firstname="Volodya", middlename="Petrovich", lastname="Volkov", nickname="Volk", tittle="Pik", company="Puki", address="gorod", homephone="9-999-99",
                             mobile="9-999-999-99-99", workphone="9-99", fax="9", email="volkov@volkov", email2="volkov2@volkov", email3="volkov3@volkov",
                             homepage="volkov0volkov", bday="21", bmonth="December", byear="2000", aday="31", amonth="July", ayear="2010", address2="gorod2", phone2="8-888-88",
                             notes="pipipuk"))
-        self.return_to_home_page(wd)
         self.logout(wd)
 
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("%s" % username)
@@ -37,6 +35,7 @@ class AddTestContact(unittest.TestCase):
         wd.find_element_by_link_text("add new").click()
 
     def create_contact(self, wd, contact):
+        self.open_add_new_contact_page(wd)
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -107,12 +106,14 @@ class AddTestContact(unittest.TestCase):
         wd.find_element_by_name("notes").send_keys("%s" % contact.notes)
         # enter new contact
         wd.find_element_by_xpath("//input[21]").click()
+        self.return_to_home_page(wd)
 
     def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
+
 
     def tearDown(self):
         self.wd.quit()
