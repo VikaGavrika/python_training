79f62776ead87dd17080332550de057bf6bf1651
veekeetee <gavriviktoriia@gmail.com>
2023-04-11 22:52:51 +0300

    add_test_group Рабочий тест 2.1 Final этап2 переход на py.test

--- AddTestGroupFX.py
+++ AddTestGroupFX.py
@@ -1,26 +1,22 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+import pytest
 from group import Group
 from application import Application
 
-class AddTestGroupFX(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_test_group_f_x(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="12345", header="54321", footer="12345"))
-        self.app.logout()
-    def test_add_test_emptygroup_f_x(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
-
-
-    def tearDown(self):
-        self.app.destroy()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+def test_add_test_group_f_x(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="12345", header="54321", footer="12345"))
+    app.logout()
+def test_add_test_emptygroup_f_x(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
 
 
-if __name__ == "__main__":
-    unittest.main()
+
