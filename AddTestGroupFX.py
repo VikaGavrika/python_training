8a7049647bab529895fe268ba78dc7489f022f0b
veekeetee <gavriviktoriia@gmail.com>
2023-04-13 11:08:20 +0300

    add_test_group Рабочий тест pytest

--- AddTestGroupFX.py
+++ AddTestGroupFX.py
@@ -11,7 +11,7 @@ def app(request):
     return fixture
 def test_add_test_group_f_x(app):
     app.login(username="admin", password="secret")
-    app.create_group(Group(name="12345", header="54321", footer="12345"))
+    app.create_group(Group(name="123456", header="654321", footer="123456"))
     app.logout()
 def test_add_test_emptygroup_f_x(app):
     app.login(username="admin", password="secret")
