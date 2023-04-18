b4ff734a6648aa96f49d061105625aaaf3e0927f
VikaGavrika <130064782+VikaGavrika@users.noreply.github.com>
2023-04-19 02:36:39 +0300

    del

--- test/test_edit_first_group.py
+++
@@ -1,6 +0,0 @@
-
-from model.group import Group
-def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group((Group(name="1111111", header="22222", footer="333333")))
-    app.session.logout()
