27e6e1159125aba19bf1f1e6e82463e972e829d1
veekeetee <gavriviktoriia@gmail.com>
2023-04-23 21:39:04 +0300

    ТЕСТ НЕ ЗАПУСКАЮТСЯ, ОШИБКА
    Даже когда убран (scope="session")
    
    Но если вместо ensure_login оставить просто login, то тест работает. Ошибка скорее всего в ensure_login

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import pytest
 from fixtura.application import Application
 
 fixture = None
-@pytest.fixture(scope="session")
+@pytest.fixture
 def app():
     global fixture
     if fixture is None:
