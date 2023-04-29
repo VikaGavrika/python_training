172b032ca830fbc8f5418a0d7cd50182e5d6424a
veekeetee <gavriviktoriia@gmail.com>
2023-04-29 16:23:31 +0300

    Добавлены в тесты проверки на сравнение длинн старого (до действия) и нового  (после действия) списка групп. Добавлен вспомогательный метод в хелпер group. Добавлен новый элемент в  конструктор model group

--- test/test_add_groupFf.py
+++ test/test_add_groupFf.py
@@ -5,10 +5,17 @@ from model.group import Group
 
 
 def test_add_group_f_f(app):
-    app.group.create(Group(name="1234567", header="654321", footer="123456"))
+    old_groups = app.group.get_group_list()
+    app.group.create(Group(name="123", header="1234", footer="12345"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+
 
 def test_add_emptygroup_f_f(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 
