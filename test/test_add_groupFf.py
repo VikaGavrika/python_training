b2a423313f8c027ea2d83c400602b0e8150afcd1
veekeetee <gavriviktoriia@gmail.com>
2023-05-18 23:11:53 +0300

    В conftest сделала загрузку информации о тестируемом приложении из конфигурационного файла, добавила  опцию командной строки, позволяющую передавать путь к этому конфигурационному файлу. Создан конфигурационный файл с данными baseUrl username password

--- test/test_add_groupFf.py
+++ test/test_add_groupFf.py
@@ -4,6 +4,7 @@ import pytest
 import random
 import string
 
+
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + " "*6
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
