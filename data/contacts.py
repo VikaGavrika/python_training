472605e2175880ec0f9f67719dd1afd348db730d
veekeetee <gavriviktoriia@gmail.com>
2023-05-24 23:32:51 +0300

    Реализовала загрузку тестовых данных из файлов в формате JSON и параметризацию тестов путём "внедрения" этих тестовых данных в фикстуру при помощи hook-функции pytest_generate_tests. (Сделала суперфункцию в conftest)
    Видоизменила тестовые данные на постоянные (не случайные)
    
    Подредактировала тесты по добавлению групп и контактов
    
    Проверила запускаются ли другие тесты - по модификации,например. Тесты работают

--- data/contacts.py
+++ data/contacts.py
@@ -2,20 +2,24 @@ from model.contact import Contact
 import random
 import string
 
-constant = [
+testdata = [
     Contact(lastname="", firstname="", address=""),
     Contact(lastname="lastname1", firstname="firstname1", address="address1")
 ]
+#constant = [
+    #Contact(lastname="", firstname="", address=""),
+    #Contact(lastname="lastname1", firstname="firstname1", address="address1")
+#]
 
-def random_string(prefix, maxlen):
+#def random_string(prefix, maxlen):
     # *1 пробел среди символов, с бОльшим кол-вом тесты не проходят проверку часто.
-    symbols = string.ascii_letters + string.digits + " "*1
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+   # symbols = string.ascii_letters + string.digits + " "*1
+   # return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [Contact(lastname="", firstname="", address="")] + [
-    Contact(lastname=random_string("lastname", 10), firstname=random_string("firstname", 8),
-            address=random_string("address", 10))
-    for i in range(2)]
+#testdata = [Contact(lastname="", firstname="", address="")] + [
+    #Contact(lastname=random_string("lastname", 10), firstname=random_string("firstname", 8),
+            #address=random_string("address", 10))
+    #for i in range(2)]
 
 #testdata = [
     #Contact(lastname="", firstname="", address=""),
