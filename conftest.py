498d0c9c77239e7644062030162bc7125d91e7b5
veekeetee <gavriviktoriia@gmail.com>
2023-05-29 22:46:38 +0300

    В конфтест добавлена обработка json структур

--- conftest.py
+++ conftest.py
@@ -4,23 +4,43 @@ import json
 from fixtura.application import Application
 import os.path
 import importlib
+import jsonpickle
+from fixtura.db import DbFixture
 
 fixture = None
 target = None
 
+def load_config(file):
+    global target
+    #код загрузки конфигурации, в качестве параметра передаем файл
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
 @pytest.fixture
 def app(request):
     global fixture
-    global target
     browser = request.config.getoption("--browser")
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-        with open(config_file) as f:
-            target = json.load(f)
+    #обращаемся к функции кода конфигурации с параметром значения опции таргет и берем блок веб из загруж конф-и
+    web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=target["baseUrl"])
-    fixture.session.ensure_login(username=target["username"], password=target["password"])
+        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
+
+@pytest.fixture(scope="session")
+def db(request):
+    # обращаемся к функции кода конфигурации с параметром значения опции таргет и берем блок db из загруж конф-и
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
+
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
@@ -37,8 +57,15 @@ def pytest_addoption(parser):
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
         if fixture.startswith("data_"):
-            testdata = load_form_module(fixture[5:])
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
-def load_form_module(module):
+def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
